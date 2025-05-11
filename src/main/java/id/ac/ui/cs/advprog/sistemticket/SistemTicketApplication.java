fdd690add69f37ea5ed12df8ac2b6afbf9091f3a
benz <ref4gha@gmail.com>
2025-05-11 17:29:39 +0700

    Init

---
+++ src/main/java/id/ac/ui/cs/advprog/sistemticket/SistemTicketApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.sistemticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemTicketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/sistemticket/SistemTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.sistemticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
