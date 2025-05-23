f524e5cd9b7e5c40bc0c6b812477883ae23e5890
benz <ref4gha@gmail.com>
2025-05-23 21:49:18 +0700

    [REFACTOR] Add TicketStatus enum

---
+++ src/main/java/id/ac/ui/cs/advprog/sistemticket/enums/TicketStatus.java
@@ -0,0 +1,26 @@
+package id.ac.ui.cs.advprog.sistemticket.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum TicketStatus {
+    AVAILABLE("AVAILABLE"),
+    PURCHASED("PURCHASED"),
+    EXPIRED("EXPIRED"),
+    USED("USED");
+
+    private final String value;
+
+    private TicketStatus(String value) {
+        this.value = value;
+    }
+
+    public static boolean contains(String params) {
+        for (TicketStatus ticketStatus : TicketStatus.values()) {
+            if (ticketStatus.value.equals(params)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
